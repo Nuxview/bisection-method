76a8223be40af34680164e8e4e0eef67365f5ed2
Shaka Maina <shakamaina74@gmail.com>
2026-07-16 20:29:42 +0300

    Initialize repo with uv init

---
+++ main.py
@@ -0,0 +1,32 @@
+def main():
+    print("Hello from bisection-method!")
+
+
+if __name__ == "__main__":
+    main()
+# Instructions
+#
+# Each group is required to implement the assigned numerical method using Python. The
+# objective is to demonstrate an understanding of the algorithm, its implementation, and
+# interpretation of the results.
+#
+# Each group should submit a report containing the following:
+# 1. A brief introduction to the assigned numerical method.
+# 2. The mathematical formulation of the method.
+# 3. The algorithm or pseudocode.
+# 4. A well-documented Python program implementing the method.
+# 5. Screenshots of the program execution.
+# 6. A table showing the iterations (where applicable).
+# 7. Appropriate plots illustrating the results (e.g., convergence plots, interpolation
+# curves, function plots, etc.).
+# 8. Interpretation and discussion of the obtained results.
+# 9. The advantages and limitations of the numerical method.
+# 10. A comparison of the obtained solution with the exact solution or Python built-in
+# numerical functions (where applicable).
+# 11. A brief conclusion.
+#
+# Bisection Method
+# Write a Python program to approximate the root of
+# f (x) = x^3 − x − 2
+# on the interval [1, 2]. Use a tolerance of 10−6 . Report the approximate root, the number
+# of iterations, and plot the function.
