5911beddda23281391dc2412463efab55f3d357e
Shaka Maina <shakamaina74@gmail.com>
2026-07-17 14:41:26 +0300

    Root calculator finished

--- main.py
+++ main.py
@@ -24,17 +24,19 @@
 # f (x) = x^3 − x − 2
 # on the interval [1, 2]. Use a tolerance of 10−6 . Report the approximate root, the number
 # of iterations, and plot the function.
+
 import numpy as np
-import matplotlib as plt
+# import matplotlib as plt
+
 
 def bisection_method(f, a, b, tol):
 
     # Check if f(a) and f(b) are of different signs
     if np.sign(f(a)) == np.sign(f(b)):
-        raise Exception ("Scalars a and b do not bound a root.")
+        raise Exception("Scalars a and b do not bound a root.")
 
     # Calculate the midpoint of a and b
-    midpoint = (a+b)/2
+    midpoint = (a + b) / 2
 
     # Case where the midpoint is the root
     if np.abs(f(midpoint)) < tol:
@@ -42,14 +44,21 @@ def bisection_method(f, a, b, tol):
 
     # Make the midpoint a or b depending on the sign
     elif np.sign(f(a)) == np.sign(f(midpoint)):
-        return bisection_method(method(f, midpoint, b, tol)
+        return bisection_method(f, midpoint, b, tol)
     elif np.sign(f(b)) == np.sign(f(midpoint)):
         return bisection_method(f, a, midpoint, tol)
 
+
 def main():
-    bisection_method()
+    f = lambda x: x**3 - x - 2
+    r1 = bisection_method(f, 1, 2, 0.1)
+    print("r1 = ", r1)
+    r01 = bisection_method(f, 1, 2, 0.01)
+    print("r01 = ", r01)
+
+    print("f(r1) = ", f(r1))
+    print("f(r01) = ", f(r01))
 
 
 if __name__ == "__main__":
     main()
-
