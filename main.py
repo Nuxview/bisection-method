ee2c84e82ac0925e2886dd1dd9e889a7bc014946
Shaka Maina <shakamaina74@gmail.com>
2026-07-20 11:51:14 +0300

    Added number of iterations to find the approximate root

--- main.py
+++ main.py
@@ -25,16 +25,16 @@
 # on the interval [1, 2]. Use a tolerance of 10−6 . Report the approximate root, the number
 # of iterations, and plot the function.
 
+# Import libraries for calculation and chart plotting
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Define the equation to solve
 def f(x):
     return x**3 - x - 2
 
 
-def bisection_method(f, a, b, tol):
-
+def bisection_method(f, a, b, tol, iterations=1):
     # Check if f(a) and f(b) are of different signs
     if np.sign(f(a)) == np.sign(f(b)):
         raise Exception("Scalars a and b do not bound a root.")
@@ -42,22 +42,26 @@ def bisection_method(f, a, b, tol):
     # Calculate the midpoint of a and b
     midpoint = (a + b) / 2
 
+    # Counter to track number of iterations
+    iterations += 1
+
     # Case where the midpoint is the root
     if np.abs(f(midpoint)) < tol:
-        return midpoint
+        return midpoint, iterations
 
     # Make the midpoint a or b depending on the sign
-    elif np.sign(f(a)) == np.sign(f(midpoint)):
-        return bisection_method(f, midpoint, b, tol)
-    elif np.sign(f(b)) == np.sign(f(midpoint)):
-        return bisection_method(f, a, midpoint, tol)
+    if np.sign(f(a)) == np.sign(f(midpoint)):
+        return bisection_method(f, midpoint, b, tol, iterations)
+    else:
+        return bisection_method(f, a, midpoint, tol, iterations)
 
-
+# Calculate values (x,y)
 def draw_chart(f):
     y_values = []
     for i in range(0, 5):
         y_values.append(f(i))
 
+    # Plot the chart using calculated (x,y) values
     plt.plot([0, 1, 2, 3, 4], y_values)
     plt.ylabel("Y-axis")
     plt.xlabel("X-axis")
@@ -66,9 +70,10 @@ def draw_chart(f):
 
 def main():
     # Pass in values to get approximate root
-    root = bisection_method(f, 1, 2, 1e-6)
-    print("Root =", root)
+    root, iterations = bisection_method(f, 1, 2, 1e-6)
+    print("Root =", round(root, 6), "\nIterations =", iterations)
 
+    # Draw the chart
     draw_chart(f)
 
 
